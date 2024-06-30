64e1f20fef31c4862bc16c117b352e1d89884efa
araraya <aryadanihlesmana@gmail.com>
2024-06-30 20:22:47 +0700

    Initial version of employee generated by generator-jhipster@7.9.4

---
+++ src/main/java/com/jhipster/employee/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.jhipster.employee.service.dto;
---
+++ src/test/java/com/jhipster/employee/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.jhipster.employee;
+
+import com.jhipster.employee.EmployeeApp;
+import com.jhipster.employee.config.AsyncSyncConfiguration;
+import com.jhipster.employee.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { EmployeeApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
